87c819278d98e12a1cd5ca5c94263b64ef343f0d
subhankar <subhankarchand66@gmail.com>
2025-08-07 11:39:24 +0530

    Hyperparameter training

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -45,7 +45,41 @@ class ModelTrainer:
                 "AdaBooost": AdaBoostRegressor(),
             }
             
-            model_report:dict=evaluate_models(x_train=x_train,y_train=y_train,x_test=x_test,y_test=y_test,models=models)
+            params = {
+    "Decision Tree": {
+        'criterion': ['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+    },
+    "Random Forest": {
+        'n_estimators': [8, 16, 32, 64, 128, 256]
+    },
+    "Gradient Boosting": {
+        'learning_rate': [.1, .01, .05, .001],
+        'subsample': [0.6, 0.7, 0.75, 0.8, 0.85, 0.9],
+        'n_estimators': [8, 16, 32, 64, 128, 256]
+    },
+    "Linear Regressor": {},  # fixed
+    "XGBClassifier": {
+        'learning_rate': [.1, .01, .05, .001],
+        'n_estimators': [8, 16, 32, 64, 128, 256]
+    },
+    "catBoosting": {
+        'depth': [6, 8, 10],
+        'learning_rate': [0.01, 0.05, 0.1],
+        'iterations': [30, 50, 100]
+    },
+    "AdaBooost": {
+        'learning_rate': [.1, .01, 0.5, .001],
+        'n_estimators': [8, 16, 32, 64, 128, 256]
+    },
+    # Optional: add for K-NN
+    "K-Neigbors classifier": {
+        'n_neighbors': [3, 5, 7, 9],
+        'weights': ['uniform', 'distance']
+    }
+}
+
+            
+            model_report:dict=evaluate_models(x_train=x_train,y_train=y_train,x_test=x_test,y_test=y_test,models=models,param=params)
             
             ## To get  best model score from dict
             best_model_score=max(sorted(model_report.values()))
